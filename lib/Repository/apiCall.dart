f0172556ae588eea7bb093ef23259ea6db7606d0
RafidKhan <rafidkhan321.rk@gmail.com>
2021-09-19 19:50:00 +0600

    declare API get request

--- lib/Repository/apiCall.dart
+++ lib/Repository/apiCall.dart
@@ -1,9 +1,22 @@
+import 'dart:convert';
+import 'package:movie_app/Models/movieModel.dart';
+import 'package:http/http.dart' as http;
+
 class ApiCall {
   int count = 10;
 
   bool loader = false;
 
+  Uri url = Uri.parse("https://salimdev.000webhostapp.com/api/movie");
 
+  Future<List<MovieModel>> getMovieData() async {
+    var response = await http.get(url);
+
+
+    loader=true;
+
+    return MovieModel.fromJsonList(json.decode(response.body));
+  }
 
 
 }
